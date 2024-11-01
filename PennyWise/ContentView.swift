d3a176166a4e74d47dac419896cc3b6c7ebe5cda
Yvonne Yang <yvnyang@icloud.com>
2024-11-01 21:46:20 +1100

    Initial Commit

---
+++ PennyWise/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PennyWise
+//
+//  Created by Yvonne Yang on 2024/11/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PennyWise/PennyWiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PennyWiseApp.swift
+//  PennyWise
+//
+//  Created by Yvonne Yang on 2024/11/1.
+//
+
+import SwiftUI
+
+@main
+struct PennyWiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
